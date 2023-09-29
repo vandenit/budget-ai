f5e60419b212b6f027a72cc714526a37c70e5ab1
Filip Van den Broeck <fvb320@gmail.com>
2023-09-29 12:00:56 +0200

    first budget

---
+++ app/api/budget.server.ts
@@ -0,0 +1,142 @@
+import { cache } from "react";
+import "server-only";
+import * as ynab from "ynab";
+
+export type Budget = {
+  id: string;
+  name: string;
+};
+
+export type Category = {
+  categoryName: string;
+  categoryId: string;
+  balance: number;
+  budgeted: number;
+  activity: number;
+};
+
+export const emptyCategory: Category = {
+  categoryName: "",
+  categoryId: "",
+  balance: 0,
+  budgeted: 0,
+  activity: 0,
+};
+export type Transaction = {
+  id: string;
+  accountName: string;
+  amount: number;
+  date: string;
+  categoryName: string;
+};
+
+export type CategoryUsage = {
+  category: string;
+  amount: number;
+};
+
+export type MonthSummary = {
+  month: string;
+  isCurrentMonth: boolean;
+  categoryUsage: Array<CategoryUsage>;
+};
+
+export async function getBudgets(): Promise<Array<Budget>> {
+  const api = new ynab.api(process.env.YNAB_ACCESS_TOKEN || "");
+  const budgets = await api.budgets.getBudgets();
+  console.log("getting budgets");
+  return budgets.data.budgets.map((budget: ynab.BudgetDetail) => ({
+    id: budget.id,
+    name: budget.name,
+  }));
+}
+
+export async function getTransactions(id: string): Promise<Array<Transaction>> {
+  const api = new ynab.api(process.env.YNAB_ACCESS_TOKEN || "");
+  const { data } = await api.transactions.getTransactions(id);
+
+  return sortMostRecentFirst(data.transactions).map(
+    (transaction: ynab.TransactionDetail) => ({
+      id: transaction.id,
+      accountName: transaction.account_name,
+      amount: transaction.amount,
+      date: transaction.date,
+      categoryId: transaction.category_id,
+      categoryName: transaction.category_name || "Uncategorized",
+    })
+  );
+}
+
+export async function getMonthSummaries(
+  id: string
+): Promise<Array<MonthSummary>> {
+  const transactions = await getTransactions(id);
+
+  return transactions.reduce(monthSummaryReducer, []);
+}
+
+export const monthSummaryReducer = (
+  acc: Array<MonthSummary>,
+  transaction: Transaction
+) => {
+  const month = transaction.date.substring(0, 7);
+  const currentMonth = new Date().toISOString().substring(0, 7);
+  const monthSummary = acc.find(
+    (summary: MonthSummary) => summary.month === month
+  );
+
+  if (monthSummary) {
+    const categoryUsage = monthSummary.categoryUsage.find(
+      (usage: CategoryUsage) => usage.category === transaction.categoryName
+    );
+
+    if (categoryUsage) {
+      categoryUsage.amount += transaction.amount;
+    } else {
+      monthSummary.categoryUsage.push({
+        category: transaction.categoryName,
+        amount: transaction.amount,
+      });
+    }
+  } else {
+    acc.push({
+      month,
+      isCurrentMonth: month === currentMonth,
+      categoryUsage: [
+        {
+          category: transaction.categoryName,
+          amount: transaction.amount,
+        },
+      ],
+    });
+  }
+
+  return acc;
+};
+
+const sortMostRecentFirst = (
+  transactions: Array<ynab.TransactionDetail>
+): Array<ynab.TransactionDetail> =>
+  transactions.sort((a, b) => {
+    return new Date(b.date).getTime() - new Date(a.date).getTime();
+  });
+
+export async function getCategories(id: string): Promise<Array<Category>> {
+  const api = new ynab.api(process.env.YNAB_ACCESS_TOKEN || "");
+
+  const { data } = await api.categories.getCategories(id);
+  const categories = data.category_groups.reduce(
+    (acc: Array<ynab.Category>, group: ynab.CategoryGroupWithCategories) => {
+      return acc.concat(group.categories);
+    },
+    []
+  );
+
+  return categories.map((category: ynab.Category) => ({
+    categoryName: category.name,
+    categoryId: category.id,
+    balance: category.balance,
+    budgeted: category.budgeted,
+    activity: category.activity,
+  }));
+}
