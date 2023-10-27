281515245a44dcac29c94484dc02db668ffc6a8e
Filip Van den Broeck <fvb320@gmail.com>
2023-10-27 12:43:14 +0200

    chartjs installation

--- app/components/budget-page/month-total-overview.tsx
+++ app/components/budget-page/month-total-overview.tsx
@@ -7,6 +7,7 @@ import {
   totalPercentageSpent,
 } from "@/app/utils/ynab";
 import { format } from "path";
+import { MonthlySpendingChart } from "../charts/mothly-spending-chart";
 
 type Props = {
   monthTotal: MonthTotal;
@@ -24,68 +25,77 @@ const MonthTotalOverview = ({
   return (
     <div className="card bg-base-100 shadow-xl mb-5">
       <div className="card-body p-0.5 sm:p-5">
-        <table className="table w-80">
-          <thead>
-            <tr>
-              <th>Total activity</th>
-              <th>Total available</th>
-              <th>Total budgeted</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>{formatYnabAmount(monthTotal.totalActivity, true)}</td>
-              <td>{formatYnabAmount(monthTotal.totalBalance)}</td>
-              <td>{formatYnabAmount(monthTotal.totalBudgeted)}</td>
-            </tr>
-            <tr>
-              <td>Month Progress</td>
-              <td colSpan={2}>
-                <progress
-                  className={`progress progress-${percentageToStatusClass(
-                    monthPercentage
-                  )}`}
-                  value={monthPercentage}
-                  max="100"
-                ></progress>
-              </td>
-            </tr>
-            <tr>
-              <td>Budget Progress</td>
-              <td colSpan={2}>
-                <progress
-                  className={`progress progress-${percentageToStatusClass(
-                    percentage
-                  )}`}
-                  value={percentage}
-                  max="100"
-                ></progress>
-              </td>
-            </tr>
-            <tr>
-              <td>Forecasted Remaining amount</td>
-              <td colSpan={2}>
-                {formatAmount(forecast.predictedRemainingAmount)}
-              </td>
-            </tr>
-            <tr>
-              <td>Forecasted Daily amount</td>
-              <td colSpan={2}>
-                {formatAmount(forecast.predictedRemainingPerDay)}
-              </td>
-            </tr>
-            <tr>
-              <td>Actual Remaing per day amount</td>
-              <td colSpan={2}>
-                {formatAmount(forecast.actualRemainingPerDay)}
-              </td>
-            </tr>
-            <tr>
-              <td>Extra suggested</td>
-              <td colSpan={2}>{formatAmount(forecast.extraAmountNeeded)}</td>
-            </tr>
-          </tbody>
-        </table>
+        <div className="flex flex-wrap w-full">
+          <div className="w-full sm:w-1/2 md:w-1/2 lg:w-1/4">
+            <table className="table w-80">
+              <thead>
+                <tr>
+                  <th>Total activity</th>
+                  <th>Total available</th>
+                  <th>Total budgeted</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>{formatYnabAmount(monthTotal.totalActivity, true)}</td>
+                  <td>{formatYnabAmount(monthTotal.totalBalance)}</td>
+                  <td>{formatYnabAmount(monthTotal.totalBudgeted)}</td>
+                </tr>
+                <tr>
+                  <td>Month Progress</td>
+                  <td colSpan={2}>
+                    <progress
+                      className={`progress progress-${percentageToStatusClass(
+                        monthPercentage
+                      )}`}
+                      value={monthPercentage}
+                      max="100"
+                    ></progress>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Budget Progress</td>
+                  <td colSpan={2}>
+                    <progress
+                      className={`progress progress-${percentageToStatusClass(
+                        percentage
+                      )}`}
+                      value={percentage}
+                      max="100"
+                    ></progress>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Forecasted Remaining amount</td>
+                  <td colSpan={2}>
+                    {formatAmount(forecast.predictedRemainingAmount)}
+                  </td>
+                </tr>
+                <tr>
+                  <td>Forecasted Daily amount</td>
+                  <td colSpan={2}>
+                    {formatAmount(forecast.predictedRemainingPerDay)}
+                  </td>
+                </tr>
+                <tr>
+                  <td>Actual Remaing per day amount</td>
+                  <td colSpan={2}>
+                    {formatAmount(forecast.actualRemainingPerDay)}
+                  </td>
+                </tr>
+                <tr>
+                  <td>Extra suggested</td>
+                  <td colSpan={2}>
+                    {formatAmount(forecast.extraAmountNeeded)}
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div className="w-full sm:w-1/2 md:w-1/2 lg:w-3/4">
+            <MonthlySpendingChart />
+          </div>
+        </div>
       </div>
     </div>
   );
