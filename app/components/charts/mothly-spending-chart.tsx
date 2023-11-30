c040e9cc9de35a0922726178389f2027666f7cc4
Filip Van den Broeck <fvb320@gmail.com>
2023-11-30 02:00:01 +0100

    axis tryout

--- app/components/charts/mothly-spending-chart.tsx
+++ app/components/charts/mothly-spending-chart.tsx
@@ -27,12 +27,31 @@ ChartJS.register(
 
 export const options = {
   responsive: true,
+  plugins: {
+    legend: {
+      position: "top" as const,
+    },
+    title: {
+      display: true,
+      text: "Spending Chart",
+    },
+  },
+  interaction: {
+    mode: "index" as const,
+    intersect: false,
+  },
   scales: {
-    y: {
-      beginAtZero: true,
+    xAxis: {
+      // Use xAxis instead of x
+      stacked: true,
+    },
+    yAxis: {
+      // Use yAxis instead of y
+      stacked: true,
     },
   },
 };
+
 type Props = {
   spendingData: MonthlySpendingData[];
   categoryId: string;
