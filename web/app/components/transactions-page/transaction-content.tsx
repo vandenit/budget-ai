2a95404852540c72494df3e249d517e5f9e596c7
Filip Van den Broeck <fvb320@gmail.com>
2024-05-10 09:57:20 +0200

    improved transaction page ux

--- web/app/components/transactions-page/transaction-content.tsx
+++ web/app/components/transactions-page/transaction-content.tsx
@@ -11,7 +11,8 @@ import {
 } from "@/app/api/transaction/transaction.utils";
 import { formatAmount } from "@/app/utils/amounts";
 import { calculateTotals, formatDate, groupByDate } from "./utils";
-import { FaArrowDown, FaArrowUp } from "react-icons/fa";
+import { FaArrowDown, FaArrowUp, FaChartBar, FaList } from "react-icons/fa";
+import { TransactionList } from "./transaction-list";
 
 const TransactionContent = ({
   budgetUuid,
@@ -26,17 +27,14 @@ const TransactionContent = ({
   categories: Category[];
   transactions: Transaction[];
 }) => {
+  const [viewMode, setViewMode] = useState("list");
   const [newCategoryId, setNewCategoryId] = useState(categoryUuid);
   const usedCategoryId = newCategoryId || categoryUuid;
-  const [openDays, setOpenDays] = useState<{ [key: string]: boolean }>(() => {
-    // Automatically open the first date
-    const firstDate = transactions.length > 0 ? transactions[0].date : null;
-    return firstDate ? { [firstDate]: true } : {};
-  });
+
   const groupedTransactions = groupByDate(transactions);
 
-  const toggleDay = (date: string) => {
-    setOpenDays((prev) => ({ ...prev, [date]: !prev[date] }));
+  const toggleView = (mode: string) => {
+    setViewMode(mode);
   };
 
   const categoryFilter = (transaction: Transaction) =>
@@ -53,7 +51,24 @@ const TransactionContent = ({
         onChange={setNewCategoryId}
       />
       <>
-        {!categoryUuid && (
+        <div className="p-4">
+          <h1 className="text-lg font-bold text-center mb-4 dark:text-white">
+            Bank Transactions
+          </h1>
+          {!categoryUuid && (
+            <div>
+              <button onClick={() => toggleView("graph")} className="p-2">
+                <FaChartBar title="Show Graph" />
+              </button>
+              <button onClick={() => toggleView("list")} className="p-2">
+                <FaList title="Show List" />
+              </button>
+            </div>
+          )}
+        </div>
+        {viewMode === "list" || categoryUuid ? (
+          <TransactionList transactions={filteredTransactions} />
+        ) : (
           <div className="m-5 w-3/4 text-center">
             <CategoryPieChart
               month={month || ""}
@@ -63,66 +78,6 @@ const TransactionContent = ({
             />
           </div>
         )}
-        <div className="p-4">
-          <h1 className="text-lg font-bold text-center mb-4 dark:text-white">
-            Bank Transactions
-          </h1>
-          {Object.entries(groupedTransactions).map(([date, transactions]) => (
-            <div key={date} className="mb-4">
-              <button
-                onClick={() => toggleDay(date)}
-                className="w-full text-left text-md font-semibold py-2 px-4 bg-gray-100 dark:bg-gray-800 rounded-lg hover:bg-gray-200 dark:hover:bg-gray-700 focus:outline-none flex justify-between items-center"
-                aria-expanded={openDays[date]}
-                aria-controls={`transactions-${date}`}
-              >
-                <span>{formatDate(date)}</span>
-                <span className="flex items-center">
-                  <FaArrowUp className="text-green-500 mr-2" />
-                  <span className="font-bold mr-4">
-                    {formatAmount(calculateTotals(transactions).income)}
-                  </span>
-                  <FaArrowDown className="text-red-500 mr-2" />
-                  <span className="font-bold">
-                    {formatAmount(calculateTotals(transactions).outcome, true)}
-                  </span>
-                </span>
-              </button>
-              {openDays[date] && (
-                <ul id={`transactions-${date}`} className="mt-2 space-y-2">
-                  {transactions.map((transaction) => (
-                    <li
-                      key={transaction.uuid}
-                      className="p-4 rounded-lg shadow bg-white dark:bg-gray-900 flex flex-col md:flex-row justify-between"
-                    >
-                      <div>
-                        <div className="font-semibold dark:text-white">
-                          {transaction.payeeName}
-                        </div>
-                        <div className="text-gray-600 dark:text-gray-300">
-                          {transaction.categoryName}
-                        </div>
-                      </div>
-                      <div className="text-right">
-                        <div
-                          className={`font-bold text-lg ${
-                            transaction.amount >= 0
-                              ? "text-green-500"
-                              : "text-red-500"
-                          } dark:text-green-200 dark:text-red-300`}
-                        >
-                          {formatAmount(transaction.amount)}
-                        </div>
-                        <div className="text-gray-500 dark:text-gray-400 text-sm">
-                          {transaction.memo}
-                        </div>
-                      </div>
-                    </li>
-                  ))}
-                </ul>
-              )}
-            </div>
-          ))}
-        </div>
       </>
     </>
   );
