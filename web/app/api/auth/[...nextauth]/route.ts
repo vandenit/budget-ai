c6feab3730cd509bbf0126c361394acd996a9db6
Filip Van den Broeck <fvb320@gmail.com>
2024-05-10 09:04:59 +0200

    fix compilation issue with auth

--- web/app/api/auth/[...nextauth]/route.ts
+++ web/app/api/auth/[...nextauth]/route.ts
@@ -1,54 +1,8 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
-import {
-  createOrUpdateUser,
-  connectUserWithYnab,
-} from "../../user/user.server";
+import NextAuth from "next-auth";
+import { connectUserWithYnab } from "../../user/user.server";
 import { syncLoggedInUser, syncUser } from "../../sync/sync.server";
+import { ynabAuthOptions } from "../../ynab/auth";
 
-export const authOptions: NextAuthOptions = {
-  providers: [
-    {
-      id: "ynab",
-      name: "YNAB",
-      type: "oauth",
-      authorization: {
-        params: {
-          scope: "read-only",
-        },
-        url: "https://app.ynab.com/oauth/authorize",
-      },
-      token: "https://app.ynab.com/oauth/token",
-      clientId: process.env.YNAB_CLIENT_ID,
-      clientSecret: process.env.YNAB_CLIENT_SECRET,
-      profile(profile) {
-        return {
-          id: "test",
-        };
-      },
-      userinfo: {
-        request: () => ({}),
-      },
-    },
-  ],
-  callbacks: {
-    async jwt({ token, account }) {
-      // Persist the OAuth access_token to the token right after signin
-      if (account && account.refresh_token) {
-        await connectUserWithYnab({
-          accessToken: account.access_token || "",
-          refreshToken: account.refresh_token || "",
-        });
-        //syncLoggedInUser();
-      }
-      return token;
-    },
-    async session({ session }) {
-      // Send properties to the client, like an access_token from a provider.
-      return session;
-    },
-  },
-};
-
-const handler = NextAuth(authOptions);
+const handler = NextAuth(ynabAuthOptions);
 
 export { handler as GET, handler as POST };
