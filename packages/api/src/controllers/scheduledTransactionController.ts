57165317586278d189a8587a63872b4ae3afd375
Filip Van den Broeck <fvb320@gmail.com>
2025-04-22 16:43:52 +0200

    fix scheduled transactions

--- packages/api/src/controllers/scheduledTransactionController.ts
+++ packages/api/src/controllers/scheduledTransactionController.ts
@@ -1,4 +1,4 @@
-import { Request } from 'express';
+import { Request, Response } from 'express';
 import * as ynabService from '../data/ynab/ynab.server';
 import { UserType } from '../data/user/user.server';
 import { getBudgetFromReq } from './budgetController';
@@ -7,7 +7,7 @@ import { getBudget } from '../data/budget/budget.server';
 
 type RequestWithUser = Request & { user: UserType };
 
-export const update = async (req: RequestWithUser) => {
+export const update = async (req: Request, res: Response) => {
     const { transactionId } = req.params;
     const updates = req.body;
     const user = await getUserFromReq(req);
@@ -25,10 +25,10 @@ export const update = async (req: RequestWithUser) => {
         transactionId,
         updates
     );
-    return result;
+    res.json(result);
 };
 
-export const remove = async (req: RequestWithUser) => {
+export const remove = async (req: RequestWithUser, res: Response) => {
     const { transactionId } = req.params;
 
     // Check if user has access to this budget
@@ -39,5 +39,5 @@ export const remove = async (req: RequestWithUser) => {
         budget.uuid,
         transactionId
     );
-    return { success: true };
+    res.json({ success: true });
 };
